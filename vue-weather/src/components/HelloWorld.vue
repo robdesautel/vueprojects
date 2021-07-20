412ecfb2d9f102337f71d474f649d37163bedc51
robdesautel <17968614+robdesautel@users.noreply.github.com>
2021-07-19 22:24:21 -0400

    Hero Work

--- vue-weather/src/components/HelloWorld.vue
+++ vue-weather/src/components/HelloWorld.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="row row-cols-3">
-    <div class="col-2">
-      <ul>
-        <LiButton v-for="name, i in buttonNames" :title="name" :key="i" class="d-flex flex-column btn btn-primary"/>
-      </ul>
+  <div>
+    <div>
+      <div class="herobackground">
+        <HeroSection/>
+      </div>
     </div>
-    <div class="col">
-      <HeroSection/>
-    </div>
-    <div class="col">
 
-    </div>
-  </div> 
+    <div class="row row-cols-3">
+
+      <div class="col-2">
+        <ul>
+          <LiButton v-for="name, i in buttonNames" :title="name" :key="i" class="d-flex flex-column btn btn-primary"/>
+        </ul>
+      </div>
+
+      <div class="col"></div>      
+    </div> 
+  </div>
 </template>
 
 <script>
-import LiButton from "./buttons/libutton.vue"
-import HeroSection from "./hero/hero.vue"
+import LiButton from "@/components/buttons/libutton.vue"
+import HeroSection from "@/components/hero/hero.vue"
 export default {
   name: 'HelloWorld',
   components: {
@@ -32,6 +37,14 @@ export default {
 </script>
 
 <style scoped>
+.herobackground{
+    /* background-color:red; */
+    background-image: url('../../public/img/partlycloudy.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 25em;
+}
+
 li {
   margin-bottom: 5%;
 }
--- vue-weather/src/components/hero/hero.vue
+++ vue-weather/src/components/hero/hero.vue
@@ -1,14 +1,9 @@
-<template>
-    <div>
-        <section>
-                <img v-for="img, i in heroimages" :src="img"  :key="i" alt="testing"/>
-                <!-- <img :src="image2"  alt="testing"/>
-                <img :src="image3"  alt="testing"/>
-                <img :src="image4"  alt="testing"/>
-                <img :src="image5"  alt="testing"/> -->
-        </section>
-    </div>   
-    
+<template >
+    <section class="row">
+            <div v-for="img, i in getImages" :key="i" class="col d-flex justify-content-center align-items-center"  > <!--class=" justify-content-evenly " -->
+                <img  :src="img"  alt="testing" class="heroImageBanner"/>
+            </div>
+    </section>
 </template>
 
 <script>
@@ -18,16 +13,56 @@ export default {
     },
         data(){
             return{
-                    heroimages:[
+            }
+        
+    },
+    computed:{
+        getImages(){
+            
+            const images = [
                     '/img/YinYangTornado.jpg',
                     '/img/SnowStorm.jpg',
                     '/img/RainyDay.jpg',
                     '/img/LightningWeatherStormThunderstorm.jpg',
                     '/img/ForestFire.png',
                     ]
-            }
-        
-    },
+            console.log(images)
+            return images
+        }
+    }
 
 }
 </script>
+
+<style scoped>
+/* .herobackground{
+    background-color:red;
+    width: 100%;
+    height: 25em;
+} */
+
+section, section div{
+    height: inherit;
+}
+section div{
+    width: 100%
+}
+
+.heroImageBanner{
+    width: 100%;
+    height: auto;
+    transition: width 2s 1s, height 2s 1s, border-radius 3s 5s, ease-in-out 1s ; ;
+}
+.heroImageBanner:hover{
+    width: 119%;
+    height: auto;
+    border-radius: 150px;
+}
+
+section div:first-child{
+    margin-left: .5%;
+}
+section div:last-child{
+    margin-right: 1.5%;
+}
+</style>
